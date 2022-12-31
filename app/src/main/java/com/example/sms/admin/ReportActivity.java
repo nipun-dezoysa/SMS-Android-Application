6e5e50d573c21ec960a1018e6cf207cf9d6ba948
San-G1 <sanjeesanji102@gmail.com>
2022-12-31 19:32:34 +0530

    Reports started

--- app/src/main/java/com/example/sms/admin/ReportActivity.java
+++ app/src/main/java/com/example/sms/admin/ReportActivity.java
@@ -6,21 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.sms.R;
 
 public class ReportActivity extends AppCompatActivity {
 
+    LinearLayout studListReport;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
-
+        studListReport =findViewById(R.id.studListReport);
         ImageView report_back = (ImageView) findViewById(R.id.report_back);
         report_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+        studListReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ReportActivity.this,StudentListReport.class);
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/com/example/sms/admin/StudentListReport.java
+++ app/src/main/java/com/example/sms/admin/StudentListReport.java
@@ -1,5 +1,6 @@
 package com.example.sms.admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -7,14 +8,47 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.sms.R;
+import com.example.sms.model.Results;
+import com.github.barteksc.pdfviewer.PDFView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.html.WebColors;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentListReport extends AppCompatActivity {
 
     ImageView report_back;
+    public static File pFile;
+    private File payfile;
+    private PDFView pdfView;
+    List<Results> results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +57,39 @@ public class StudentListReport extends AppCompatActivity {
 
         report_back = findViewById(R.id.report_back);
 
-        report_back.setOnClickListener(new View.OnClickListener() {
+//        report_back.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onBackPressed();
+//            }
+//        });
+
+        pdfView = findViewById(R.id.payment_pdf_viewer);
+
+
+        Button reportButton = findViewById(R.id.button_disable_report);
+        reportButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                onBackPressed();
+            public void onClick(View view) {
+                previewDisabledUsersReport(view);
+
             }
         });
+
+        //
+        results = new ArrayList<>();
+
+        //create files in charity care folder
+        payfile = new File("/storage/emulated/0/Report/");
+
+        //check if they exist, if not create them(directory)
+        if ( !payfile.exists()) {
+            payfile.mkdirs();
+        }
+        pFile = new File(payfile, "PaymentUsers.pdf");
+
+        //fetch payment and disabled users details;
+        fetchPaymentUsers();
 
     }
 
@@ -47,10 +108,148 @@ public class StudentListReport extends AppCompatActivity {
         }
         return true;
     }
-    public void previewDisabledUsersReport()
+    private void createPaymentReport(  List<Results> results) throws DocumentException, FileNotFoundException {
+        BaseColor colorWhite = WebColors.getRGBColor("#ffffff");
+        BaseColor colorBlue = WebColors.getRGBColor("#056FAA");
+        BaseColor grayColor = WebColors.getRGBColor("#425066");
+
+
+
+        Font white = new Font(Font.FontFamily.HELVETICA, 15.0f, Font.BOLD, colorWhite);
+        FileOutputStream output = new FileOutputStream(pFile);
+        Document document = new Document(PageSize.A4);
+        PdfPTable table = new PdfPTable(new float[]{6, 25, 20, 20});
+        table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.getDefaultCell().setFixedHeight(50);
+        table.setTotalWidth(PageSize.A4.getWidth());
+        table.setWidthPercentage(100);
+        table.getDefaultCell().setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        Chunk noText = new Chunk("No.", white);
+        PdfPCell noCell = new PdfPCell(new Phrase(noText));
+        noCell.setFixedHeight(50);
+        noCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        noCell.setVerticalAlignment(Element.ALIGN_CENTER);
+
+        Chunk nameText = new Chunk("Student Name", white);
+        PdfPCell nameCell = new PdfPCell(new Phrase(nameText));
+        nameCell.setFixedHeight(50);
+        nameCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        nameCell.setVerticalAlignment(Element.ALIGN_CENTER);
+
+        Chunk phoneText = new Chunk("Total Marks", white);
+        PdfPCell phoneCell = new PdfPCell(new Phrase(phoneText));
+        phoneCell.setFixedHeight(50);
+        phoneCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        phoneCell.setVerticalAlignment(Element.ALIGN_CENTER);
+
+        Chunk amountText = new Chunk("Grades", white);
+        PdfPCell amountCell = new PdfPCell(new Phrase(amountText));
+        amountCell.setFixedHeight(50);
+        amountCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        amountCell.setVerticalAlignment(Element.ALIGN_CENTER);
+
+
+        Chunk footerText = new Chunk("Moses Njoroge - Copyright @ 2020");
+        PdfPCell footCell = new PdfPCell(new Phrase(footerText));
+        footCell.setFixedHeight(70);
+        footCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        footCell.setVerticalAlignment(Element.ALIGN_CENTER);
+        footCell.setColspan(4);
+
+
+        table.addCell(noCell);
+        table.addCell(nameCell);
+        table.addCell(phoneCell);
+        table.addCell(amountCell);
+        table.setHeaderRows(1);
+
+        PdfPCell[] cells = table.getRow(0).getCells();
+
+
+        for (PdfPCell cell : cells) {
+            cell.setBackgroundColor(grayColor);
+        }
+        for (int i = 0; i < results.size(); i++) {
+            Results res = results.get(i);
+
+            String id = String.valueOf(i + 1);
+            //Todo : set the data from the results here
+            String name = res.getUsername();
+            Float marks = res.getTotal();
+            String grade = res.getGrades();
+
+            //IF
+
+            table.addCell(id + ". ");
+            table.addCell(name);
+            table.addCell(marks+"");
+            table.addCell(grade);
+
+        }
+
+        PdfPTable footTable = new PdfPTable(new float[]{6, 25, 20, 20});
+        footTable.setTotalWidth(PageSize.A4.getWidth());
+        footTable.setWidthPercentage(100);
+        footTable.addCell(footCell);
+
+        PdfWriter.getInstance(document, output);
+        document.open();
+        Font g = new Font(Font.FontFamily.HELVETICA, 25.0f, Font.NORMAL, grayColor);
+        document.add(new Paragraph(" How to generate real-time reports using Firebase\n\n", g));
+        document.add(table);
+        document.add(footTable);
+
+        document.close();
+    }
+
+    //function to fetch payment data from the database
+    private void fetchPaymentUsers()
     {
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Results").child("2023").child("Term 1").child("10").child("Maths");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Results res = snapshot.getValue(Results.class);
+                    results.add(res);
+                }
+                //create a pdf file and catch exception beacause file may not be created
+                try {
+                    createPaymentReport(results);
+                } catch (DocumentException | FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        });
+
+    }
+
+
+    private void DisplayReport()
+    {
+        pdfView.fromFile(pFile)
+                .pages(0,2,1,3,3,3)
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .load();
+
+
+    }
+
+    public void previewDisabledUsersReport(View view) {
         if (hasPermissions(this, PERMISSIONS)) {
-//            DisplayReport();
+            DisplayReport();
         } else {
             ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         }
